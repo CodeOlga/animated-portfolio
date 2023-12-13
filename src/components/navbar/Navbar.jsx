7182e20644d9dad60226ad9026e9172ea6a63d5f
CodeOlga <111949555+CodeOlga@users.noreply.github.com>
2023-12-13 20:46:20 +0200

    change meta

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -13,10 +13,10 @@ const Navbar = () => {
           transition={{ duration: 0.5 }}
         >Olga K</motion.span>
         <div className="social">
-          <a href='#'><img src='/facebook.png' alt='' /></a>
-          <a href='#'><img src='/instagram.png' alt='' /></a>
-          <a href='#'><img src='/youtube.png' alt='' /></a>
-          <a href='#'><img src='/dribbble.png' alt='' /></a>
+          <a href='#'><img src='/facebook.png' alt='facebook' /></a>
+          <a href='#'><img src='/instagram.png' alt='instagram' /></a>
+          <a href='#'><img src='/youtube.png' alt='youtube' /></a>
+          <a href='#'><img src='/dribbble.png' alt='dribbble' /></a>
         </div>
       </div>
     </div>
