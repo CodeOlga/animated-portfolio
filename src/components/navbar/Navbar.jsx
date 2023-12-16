4ad9f13d99f5ddaa5a8d53b4f708f6f57530bded
CodeOlga <111949555+CodeOlga@users.noreply.github.com>
2023-12-16 11:51:18 +0200

    update icons

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -13,13 +13,25 @@ const Navbar = () => {
           transition={{ duration: 0.5 }}
         >Olga K</motion.span>
         <div className="social">
-          <a href='#'><img src='/facebook.png' alt='facebook' /></a>
+          {/* <a href='#'><img src='/facebook.png' alt='facebook' /></a>
           <a href='#'><img src='/instagram.png' alt='instagram' /></a>
           <a href='#'><img src='/youtube.png' alt='youtube' /></a>
-          <a href='#'><img src='/dribbble.png' alt='dribbble' /></a>
+          <a href='#'><img src='/dribbble.png' alt='dribbble' /></a> */}
+
+          <a href='#'><img src='/twitter_icon.svg' alt='twitter' /></a>
+          <a href='#'><img src='/instagram_icon.svg' alt='instagram' /></a>
+          <a href='#'><img src='/facebook_icon.svg' alt='facebook' /></a>
+          <a href='#'><img src='/github_icon1.svg' alt='github' /></a>
+          <a href='#'><img src='/linkedin_icon.svg' alt='linkedin' /></a>
         </div>
       </div>
     </div>
   )
 }
-export default Navbar
+export default Navbar;
+
+
+
+
+
+
